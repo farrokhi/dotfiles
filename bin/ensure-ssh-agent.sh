cd49ab73d12f421a86678c89f3648f97d68dbd68
Babak Farrokhi <babak@farrokhi.net>
2019-10-25 12:51:04 +0200

    refactor

--- bin/ensure-ssh-agent.sh
+++ bin/ensure-ssh-agent.sh
@@ -1,10 +1,10 @@
-AGENT_SCRIPT=${HOME}/load-agent.sh
+AGENT_SCRIPT=${HOME}/bin/load-agent.sh
 
 while true; do
     [ -r ${AGENT_SCRIPT} ] && source ${AGENT_SCRIPT}
-    if [ -n "$SSH_AGENT_PID" ]; then 
+    if [ -n "${SSH_AGENT_PID}" ]; then 
         # ensure the variable actually reflects a running ssh-agent process
-        ps -p $SSH_AGENT_PID | grep -q ssh-agent$ && break
+        ps -p ${SSH_AGENT_PID} | grep -q ssh-agent$ && break
     fi
     # SSH_AGENT_PID is either not set or has stale PID
     echo "Running ssh-agent"
