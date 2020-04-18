af79dbc4645f80178fbd432a8d810f2e184f38da
Babak Farrokhi <babak@farrokhi.net>
2020-04-18 22:17:28 +0200

    Small improvements

--- bin/gpgcleanup.sh
+++ bin/gpgcleanup.sh
@@ -3,9 +3,9 @@
 # blog.lavall.ee
 
 echo -n "Expired Keys: "
-for expiredKey in $(gpg2 --list-keys | awk '/^pub.* \[expired\: / {id=$2; sub(/^.*\//, "", id); print id}' | fmt -w 999 ); do
+for expiredKey in $(gpg -k | gawk '/^pub.* \[expired: / {id=$2; sub(/^.*\//, "", id); print id}' | fmt -w 999 ); do
     echo -n "$expiredKey"
-    gpg2 --batch --yes --quiet --delete-keys $expiredKey >/dev/null 2>&1
+    gpg --batch --quiet --yes --delete-keys $expiredKey >/dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo -n "(OK), "
     else
@@ -17,7 +17,7 @@ echo done.
 echo -n "Update Keys: "
 for keyid in $(gpg -k | grep ^pub | grep -v expired: | grep -v revoked: | cut -d/ -f2 | cut -d' ' -f1); do
     echo -n "$keyid"
-    gpg2 --batch --quiet --edit-key "$keyid" check clean cross-certify save quit > /dev/null 2>&1
+    gpg --batch --quiet --edit-key "$keyid" check clean cross-certify save quit > /dev/null 2>&1
     if [ $? -eq 0 ]; then
         echo -n "(OK), "
     else
@@ -26,10 +26,11 @@ for keyid in $(gpg -k | grep ^pub | grep -v expired: | grep -v revoked: | cut -d
 done
 echo done.
 
-gpg2 --batch --quiet --refresh-keys > /dev/null 2>&1
+echo -n "Refreshing Keys: "
+gpg --batch --quiet --refresh-keys > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    echo "Refresh OK"
+    echo "Successful"
 else
-     echo "Refresh FAIL."
+     echo "Failed"
 fi
 
