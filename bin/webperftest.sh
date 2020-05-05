b273844acec6473acf33320acb210f2859fabe94
Babak Farrokhi <babak@farrokhi.net>
2020-05-05 13:01:06 +0200

    Tidy up

--- bin/webperftest.sh
+++ bin/webperftest.sh
@@ -7,13 +7,12 @@ DELAY=${2}
 
 trap 'echo "Cleaning up"; wait ; exit' INT TERM EXIT
 
-
 [ -z ${WEBSITE} ] && exit 1
 [ -z ${DELAY} ] && DELAY=1
 
 while :
-	do
-		curl -s -o /dev/null -w "Connect: %{time_connect}\t TTFB: %{time_starttransfer}\t Total time: %{time_total} \n" ${1} 
-		sleep ${DELAY}
-	done
+    do
+        curl -s -o /dev/null -w "Connect: %{time_connect}\t TTFB: %{time_starttransfer}\t Total time: %{time_total} \n" ${1} 
+        sleep ${DELAY}
+    done
 
