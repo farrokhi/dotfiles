dd97674dd37ee1368fb1094a4edcdbb1970f2296
Babak Farrokhi <babak@farrokhi.net>
2020-11-08 11:10:33 +0100

    Improved mail syncing
    - Build folder list on the fly during sync using syncmail script
    - Add macros for better folder navigation in sidebar

---
+++ .mutt/syncmail.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Populate a list of folder for mutt
+mbsync -l -a | egrep -v '^(Snoozed$|Notes$|Calendar$|Tasks$|Journal$|Contacts$|Old Archive)' | awk '{printf "\"=%s\" ", $1}' > ~/Mail/mailboxes
+
+# Synchronize mailbox
+mbsync -a
