a46eea97d50d74a61e3d08f46808da2ec2df0201
Babak Farrokhi <babak@farrokhi.net>
2020-11-09 21:44:12 +0100

    Proper handling of space in folder names

--- .mutt/syncmail.sh
+++ .mutt/syncmail.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Populate a list of folder for mutt
-mbsync -l -a | egrep -v '^(Snoozed$|Notes$|Calendar$|Tasks$|Journal$|Contacts$|Old Archive)' | awk '{printf "\"=%s\" ", $1}' > ~/Mail/mailboxes
+mbsync -l -a | egrep -v '^(Snoozed$|Notes$|Calendar$|Tasks$|Journal$|Contacts$|Old Archive)' | awk '{printf "\"=%s\" ", $0}' > ~/Mail/mailboxes
 
 # Synchronize mailbox
 mbsync -a
