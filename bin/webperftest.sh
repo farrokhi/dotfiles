38a5e552ab4ee2941a48691f7097155f28a0b9be
Babak Farrokhi <babak@farrokhi.net>
2020-11-24 21:40:42 +0100

    Add more details

--- bin/webperftest.sh
+++ bin/webperftest.sh
@@ -10,9 +10,8 @@ trap 'echo "Cleaning up"; wait ; exit' INT TERM EXIT
 [ -z ${WEBSITE} ] && exit 1
 [ -z ${DELAY} ] && DELAY=1
 
-while :
-    do
-        curl -s -o /dev/null -w "Connect: %{time_connect}\t TTFB: %{time_starttransfer}\t Total time: %{time_total} \n" ${1} 
-        sleep ${DELAY}
-    done
+while :; do
+    curl -s -o /dev/null -w "Lookup: %{time_namelookup}\t Connect: %{time_connect}\t Pre-transfer: %{time_pretransfer}\t TTFB: %{time_starttransfer}\t Total time: %{time_total} \n" ${1} 
+    sleep ${DELAY}
+done
 
