c2972cb73df4602a394b4bbff71e1990393ce5a4
Babak Farrokhi <babak@farrokhi.net>
2021-07-04 11:00:51 +0200

    Check for main branch

--- bin/update-all.sh
+++ bin/update-all.sh
@@ -12,11 +12,11 @@ do
 		svn update 
 		cd ..
 		echo
-	elif [ -d "$dir/.git" ];
+	elif [ -e "$dir/.git" ];
 	then
 		echo "====== Updating $dir ..."
 		cd $dir
-		git pull origin
+                git branch -l | grep -q master && git pull origin master || git pull origin main
 		cd ..
 		echo
 	elif [ -d "$dir/.hg" ];
