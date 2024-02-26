6bd6e0b5098c3ea829ebca60ac2d23ca7e205e84
Babak Farrokhi <babak@farrokhi.net>
2024-02-26 10:20:20 +0100

    Improve terminal configuration
    - Use MacOS native fullscreen mode
    - Improve pane management UX
    - Default to GPU acceleration

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,6 +12,13 @@ end
 
 -- This is where you actually apply your config choices
 
+native_macos_fullscreen_mode = false
+
+config.inactive_pane_hsb = {
+  saturation = 0.9,
+  brightness = 0.6,
+}
+
 -- Color Scheme
 config.color_scheme = 'Gruvbox dark, hard (base16)'
 config.colors = {
@@ -24,16 +31,21 @@ config.cursor_thickness = "1px"
 -- Font
 config.font = wezterm.font {
   family = 'Berkeley Mono',
-  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
+  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0', 'dlig=0' },
 }
 
 config.font_size = 16.0
 config.line_height = 1.0
 
+front_end = "OpenGL"
+
+-- Tab Bar
+config.use_fancy_tab_bar = false
+config.enable_tab_bar = true
+
 -- Window Style
 config.hide_tab_bar_if_only_one_tab = true
-config.window_decorations="TITLE | RESIZE | MACOS_FORCE_DISABLE_SHADOW"
-config.enable_tab_bar = true
+-- config.window_decorations="TITLE | RESIZE | MACOS_FORCE_DISABLE_SHADOW"
 config.initial_rows = 35
 config.initial_cols = 120
 config.audible_bell="Disabled"
@@ -43,7 +55,6 @@ config.window_padding = {
   top = '0.5cell',
   bottom = '0.5cell',
 }
-
 
 -- and finally, return the configuration to wezterm
 return config
